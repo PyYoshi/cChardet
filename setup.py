565c13ba94c337af62814e4c1854ffc7c42257d8
PyYoshi <yohihiro.m@gmail.com>
2012-06-20 21:45:57 +0900

    support for linux

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 from distutils.core import setup, Extension
+import platform,os,sys
 from Cython.Distutils import build_ext # use cython's buld_ext
-
-charsetdetect_include_dir = r"."
-charsetdetect_lib_dir = r"libcharsetdetect.dll"
 
 setup(
     name = 'cchardet',
+    url = r"https://github.com/PyYoshi/cChardet",
     description = 'Universal encoding detector',
     version = '0.1',
     cmdclass = {'build_ext': build_ext},
     ext_modules = [
         Extension("cchardet",
             sources = ["cchardet.pyx"],
-            include_dirs=[charsetdetect_include_dir,],
-            extra_objects=[charsetdetect_lib_dir,],
+            libraries=['charsetdetect'],
             language="c",
         ),
 
