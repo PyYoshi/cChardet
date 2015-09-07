2ac7e55d1f0fae83a925ac16c29326571081b9ba
PyYoshi <myoshi321go@gmail.com>
2015-09-07 22:10:41 +0900

    add windows build/upload scripts

---
+++ win_upload.bat
@@ -0,0 +1,4 @@
+py -3.4-32 -m twine upload dist/cchardet-*-cp34-none-win32.whl
+py -3.4-x64 -m twine upload dist/cchardet-*-cp34-none-win_amd64.whl
+py -2.7-32 -m twine upload dist/cchardet-*-cp27-none-win32.whl
+py -2.7-x64 -m twine upload dist/cchardet-*-cp27-none-win_amd64.whl
