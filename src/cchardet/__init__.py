fe7775336283f460828ec54baf1164e2b645aef9
Denis Matiychuk <moden.py@yandex.ru>
2016-10-16 23:31:47 +0300

    Partially-feed interface

--- src/cchardet/__init__.py
+++ src/cchardet/__init__.py
@@ -17,3 +17,27 @@ def detect(msg):
     if isinstance(encoding, bytes):
         encoding = encoding.decode()
     return {"encoding": encoding, "confidence": confidence}
+
+
+class Detector(object):
+    """Wrap csd_consider with 'feed' feature."""
+
+    def __init__(self):
+        self._detector = _cchardet.Detector()
+
+    def feed(self, data):
+        self._detector.feed(data)
+
+    def close(self):
+        self._detector.close()
+
+    @property
+    def done(self):
+        return self._detector.done
+
+    @property
+    def result(self):
+        encoding, confidence = self._detector.result
+        if isinstance(encoding, bytes):
+            encoding = encoding.decode()
+        return {"encoding": encoding, "confidence": confidence}
