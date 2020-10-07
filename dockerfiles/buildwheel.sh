5fee5cd335f7c44364edef6f8e11e6cd66ef6202
Marat Sharafutdinov <decaz89@gmail.com>
2020-10-07 12:56:06 +0300

    Support Python 3.9

--- dockerfiles/buildwheel.sh
+++ dockerfiles/buildwheel.sh
@@ -4,7 +4,7 @@ set -e -x
 ARCH=`uname -p`
 echo "arch=$ARCH"
 
-for V in cp38-cp38 cp37-cp37m cp36-cp36m cp35-cp35m; do
+for V in cp39-cp39 cp38-cp38 cp37-cp37m cp36-cp36m cp35-cp35m; do
     PYBIN=/opt/python/$V/bin
     rm -rf build src/cchardet/__pycache__ src/cchardet/*.cpp src/cchardet/*.pyc src/cchardet/*.so src/cchardet.egg-info src/tests/__pycache__ src/tests/*.pyc
     $PYBIN/pip install -r requirements-dev.txt
