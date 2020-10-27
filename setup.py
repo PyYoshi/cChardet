c4c37ab04ea7f7d9ac5b578ab89a09131ca99c72
Marat Sharafutdinov <decaz89@gmail.com>
2020-10-27 11:05:24 +0300

    Deprecate Python 3.5

--- setup.py
+++ setup.py
@@ -129,7 +129,6 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
