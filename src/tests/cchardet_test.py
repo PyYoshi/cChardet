e9ca8644a5de6655722540cae2bf56261e2174a8
q0w <43147888+q0w@users.noreply.github.com>
2022-07-19 11:17:02 +0300

    Undo reformat

--- src/tests/cchardet_test.py
+++ src/tests/cchardet_test.py
@@ -4,49 +4,43 @@ import os
 import cchardet
 
 SKIP_LIST = [
-    "src/tests/testdata/ja/utf-16le.txt",
-    "src/tests/testdata/ja/utf-16be.txt",
-    "src/tests/testdata/es/iso-8859-15.txt",
-    "src/tests/testdata/da/iso-8859-1.txt",
-    "src/tests/testdata/he/iso-8859-8.txt",
+    'src/tests/testdata/ja/utf-16le.txt',
+    'src/tests/testdata/ja/utf-16be.txt',
+    'src/tests/testdata/es/iso-8859-15.txt',
+    'src/tests/testdata/da/iso-8859-1.txt',
+    'src/tests/testdata/he/iso-8859-8.txt',
 ]
 
 # Python can't decode encoding
 SKIP_LIST_02 = [
-    "src/tests/testdata/vi/viscii.txt",
-    "src/tests/testdata/zh/euc-tw.txt",
+    'src/tests/testdata/vi/viscii.txt',
+    'src/tests/testdata/zh/euc-tw.txt',
 ]
 SKIP_LIST_02.extend(SKIP_LIST)
 
 
 def test_ascii():
-    detected_encoding = cchardet.detect(b"abcdefghijklmnopqrstuvwxyz")
-    assert "ascii" == detected_encoding["encoding"].lower()
+    detected_encoding = cchardet.detect(b'abcdefghijklmnopqrstuvwxyz')
+    assert 'ascii' == detected_encoding['encoding'].lower()
 
 
 def test_detect():
-    testfiles = glob.glob("src/tests/testdata/*/*.txt")
+    testfiles = glob.glob('src/tests/testdata/*/*.txt')
     for testfile in testfiles:
         if testfile.replace("\\", "/") in SKIP_LIST:
             continue
 
         base = os.path.basename(testfile)
         expected_charset = os.path.splitext(base)[0]
-        with open(testfile, "rb") as f:
+        with open(testfile, 'rb') as f:
             msg = f.read()
             detected_encoding = cchardet.detect(msg)
-            assert (
-                expected_charset.lower()
-                == detected_encoding["encoding"].lower()
-            )
+            assert expected_charset.lower() == detected_encoding['encoding'].lower()
 
 
 def test_detector():
     detector = cchardet.UniversalDetector()
-    with open(
-        "src/tests/samples/wikipediaJa_One_Thousand_and_One_Nights_SJIS.txt",
-        "rb",
-    ) as f:
+    with open("src/tests/samples/wikipediaJa_One_Thousand_and_One_Nights_SJIS.txt", 'rb') as f:
         line = f.readline()
         while line:
             detector.feed(line)
@@ -55,14 +49,14 @@ def test_detector():
             line = f.readline()
     detector.close()
     detected_encoding = detector.result
-    assert "shift_jis" == detected_encoding["encoding"].lower()
+    assert "shift_jis" == detected_encoding['encoding'].lower()
 
 
 def test_github_issue_20():
     """
     https://github.com/PyYoshi/cChardet/issues/20
     """
-    msg = b"\x8f"
+    msg = b'\x8f'
 
     cchardet.detect(msg)
 
@@ -72,24 +66,24 @@ def test_github_issue_20():
 
 
 def test_decode():
-    testfiles = glob.glob("src/tests/testdata/*/*.txt")
+    testfiles = glob.glob('src/tests/testdata/*/*.txt')
     for testfile in testfiles:
         if testfile.replace("\\", "/") in SKIP_LIST_02:
             continue
 
         base = os.path.basename(testfile)
         expected_charset = os.path.splitext(base)[0]
-        with open(testfile, "rb") as f:
+        with open(testfile, 'rb') as f:
             msg = f.read()
-            detected_encoding = cchardet.detect(msg)
-            try:
-                msg.decode(detected_encoding["encoding"])
-            except LookupError as e:
-                print(
-                    "LookupError: { file=%s, encoding=%s }"
-                    % (testfile, detected_encoding["encoding"]),
-                )
-                raise e
+        detected_encoding = cchardet.detect(msg)
+        try:
+            msg.decode(detected_encoding["encoding"])
+        except LookupError as e:
+            print(
+                "LookupError: { file=%s, encoding=%s }"
+                % (testfile, detected_encoding["encoding"]),
+            )
+            raise e
 
 
 def test_utf8_with_bom():
